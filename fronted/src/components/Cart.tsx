bffc4b5983f04487063fcefe8534213f8544e82f
hoang cuong <vancuong25112003@gmail.com>
2025-01-03 14:41:17 +0700

    update responsive

--- fronted/src/components/Cart.tsx
+++ fronted/src/components/Cart.tsx
@@ -6,6 +6,7 @@ import { FaShoppingBasket } from "react-icons/fa";
 import { MdDeleteForever } from "react-icons/md";
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { AiOutlineMenu } from 'react-icons/ai';
 
 const Cart = ({ show, setShow }) => {
 
@@ -55,24 +56,56 @@ const Cart = ({ show, setShow }) => {
         <div className='flex flex-col w-full bg-gray-100 shadow-md px-3 py-3'>
             {cart.length != 0 ?
                 <div className=' mt-3 flex flex-col gap-1.5'>
+                    <div
+                        className='flex md:hidden items-center gap-3 mb-3 cursor-pointer'
+                        onClick={() => setShow(!show)}
+                    >
+                        <AiOutlineMenu />
+                        <p>Menu</p>
+                    </div>
                     <div className='flex gap-3.5 items-center'>
                         <FaShoppingBasket className='text-3xl text-gray-600' />
                         <p className='text-xl font-semibold'>Giỏ hàng của bạn</p>
                     </div>
-                    <div className='mt-3.5 grid grid-cols-[50%_13%_17%_13%_7%] justify-center bg-gray-200 shadow-md px-3 py-2'>
+                    <div className='mt-3.5 flex lg:grid lg:grid-cols-[50%_13%_17%_13%_7%] lg:justify-center bg-gray-200 shadow-md px-3 py-2'>
                         <p className='text-lg font-semibold'>Tất cả ({cart.length} sản phẩm)</p>
-                        <p className='text-lg font-semibold text-center'>Giá</p>
-                        <p className='text-lg font-semibold text-center'>Số lượng</p>
-                        <p className='text-lg font-semibold text-center'>Thành tiền</p>
+                        <p className='text-lg font-semibold text-center hidden lg:block'>Giá</p>
+                        <p className='text-lg font-semibold text-center hidden lg:block'>Số lượng</p>
+                        <p className='text-lg font-semibold text-center hidden lg:block'>Thành tiền</p>
                     </div>
                     {cart?.map((i, index) => (
-                        <div key={index} className='mt-3.5 grid items-center text-center grid-cols-[50%_13%_17%_13%_7%] px-3 py-2'>
+                        <div key={index} className='mt-3.5 flex lg:grid items-center text-center lg:grid-cols-[50%_13%_17%_13%_7%] px-3 py-2'>
                             <div className='flex gap-5 items-center'>
                                 <img src={i?.product?.image} className='w-28  ' alt="" />
-                                <p>{i?.product?.name}</p>
+                                <p className='lg:block hidden'>{i?.product?.name}</p>
+                                <div className='lg:hidden flex flex-col gap-1 text-sm'>
+                                    <p className='text-start'>{i?.product?.name}</p>
+                                    <div className='flex justify-start items-center gap-3.5'>
+                                        <p
+                                            className='text-xl cursor-pointer py-0.5 w-7 rounded-full bg-gray-100 shadow-md'
+                                            onClick={() => decreaseQuantity(i?.product?._id)}
+                                        >
+                                            -
+                                        </p>
+                                        <p className='px-5 py-2 text-center font-semibold bg-gray-100 rounded-md  shadow-md'>{i?.quantity}</p>
+                                        <p
+                                            className='text-xl cursor-pointer py-0.5 w-7 rounded-full bg-gray-100 shadow-md'
+                                            onClick={() => increaseQuantity(i?.product?._id)}
+                                        >
+                                            +
+                                        </p>
+                                    </div>
+                                    <div className='flex gap-3.5'>
+                                        <p className='mx-3.5'>{formatMoney(i?.product?.newPrice * i?.quantity)} vnd</p>
+                                        <MdDeleteForever
+                                            className='text-gray-700 text-2xl cursor-pointer'
+                                            onClick={() => removeFromCart(i?.product?._id)}
+                                        />
+                                    </div>
+                                </div>
                             </div>
-                            <p>{formatMoney(i?.product?.newPrice)} vnd</p>
-                            <div className='flex justify-center items-center gap-3.5'>
+                            <p className='lg:block hidden'>{formatMoney(i?.product?.newPrice)} vnd</p>
+                            <div className='lg:flex hidden justify-center items-center gap-3.5'>
                                 <p
                                     className='text-xl cursor-pointer py-0.5 w-7 rounded-full bg-gray-100 shadow-md'
                                     onClick={() => decreaseQuantity(i?.product?._id)}
@@ -87,9 +120,9 @@ const Cart = ({ show, setShow }) => {
                                     +
                                 </p>
                             </div>
-                            <p>{formatMoney(i?.product?.newPrice * i?.quantity)} vnd</p>
+                            <p className='lg:block hidden'>{formatMoney(i?.product?.newPrice * i?.quantity)} vnd</p>
                             <MdDeleteForever
-                                className='text-gray-700 text-2xl cursor-pointer'
+                                className='text-gray-700 text-2xl cursor-pointer lg:block hidden'
                                 onClick={() => removeFromCart(i?.product?._id)}
                             />
                         </div>
@@ -100,6 +133,13 @@ const Cart = ({ show, setShow }) => {
                     </div>
                 </div>
                 : <div className=' mt-3 flex md:flex-row flex-col gap-3.5'>
+                    <div
+                        className='flex md:hidden items-center gap-3 mb-3 cursor-pointer'
+                        onClick={() => setShow(!show)}
+                    >
+                        <AiOutlineMenu />
+                        <p>Menu</p>
+                    </div>
                     <img src={empty} className='size-72' alt="" />
 
                     <div className='flex flex-col'>
