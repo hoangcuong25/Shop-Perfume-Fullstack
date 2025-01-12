4e7b757dc82b756051172f64635b7feb1693282a
hoàng văn cường <160385792+hoangcuong25@users.noreply.github.com>
2025-01-12 19:55:32 +0700

    Update Context.tsx

--- admin/src/context/Context.tsx
+++ admin/src/context/Context.tsx
@@ -8,7 +8,7 @@ const AppContextProvider = (props) => {
 
     const [token, setToken] = useState(localStorage.getItem('atoken') ? localStorage.getItem('atoken') : false)
 
-    const backendUrl = 'http://localhost:4000'
+    const backendUrl = 'https://shop-perfume-fullstack.onrender.com'
 
     const [users, setUsers] = useState([])
     const [products, setProducts] = useState([])
